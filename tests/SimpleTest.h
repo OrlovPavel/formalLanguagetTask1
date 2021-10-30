a8c049c734cbd894b2af0e85ff45e25a18bba79c
OrlovPavel <orlov.pg@phystech.edu>
2021-10-30 18:42:22 +0300

    тесты

---
+++ tests/SimpleTest.h
@@ -0,0 +1,12 @@
+#ifndef FORMALLANGTASK1_SIMPLETEST_H
+#define FORMALLANGTASK1_SIMPLETEST_H
+
+#pragma once
+
+#include "gtest/gtest.h"
+
+class SimpleTest : public ::testing::Test {
+
+};
+
+#endif //FORMALLANGTASK1_SIMPLETEST_H
